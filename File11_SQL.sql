0e58ddba449333da2a2b8e9891929b924544409c
Karthik Sagar <karthiksagar9999@gmail.com>
2019-02-27 23:31:15 -0600

    Triangle_Check_Query

--- File11_SQL.sql
+++ File11_SQL.sql
@@ -1,3 +1,3 @@
 SELECT CASE WHEN A + B > C AND A + C > B AND B + C > A THEN CASE WHEN A = B AND B = C THEN 'Equilateral' WHEN A = B OR B = C OR A = C THEN 'Isosceles' WHEN A != B OR B != C OR A != C THEN 'Scalene' END ELSE 'Not A Triangle' END FROM TRIANGLES;
 
-// Here A, B and C are the sides of a triangle , The table is given as TRIANGLE.jpg in the files
+// Here A, B and C are the sides of a triangle , The table is given as TRIANGLES.jpg in the files
